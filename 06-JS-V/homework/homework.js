94bb1a0c54b833edb54c67e7b3f3d9eb5479ace2
JavierAlex <javier.quispe@tecsup.edu.pe>
2022-08-05 09:39:52 -0500

    avance 6 y 7

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -18,27 +18,27 @@ function crearUsuario() {
   //  return 'Hola, mi nombre es ' + this.nombre
   //}
   //return Usuario
-  class Usuario{
-    constructor(opciones){
-      this.usuario = opciones.usuario
-      this.nombre = opciones.nombre
-      this.email = opciones.email
-      this.password = opciones.password
-      this.saludar = function (){
-        return 'Hola, mi nombre es ' + this.nombre
-      }
+  class Usuario {
+    constructor(opciones) {
+      this.usuario = opciones.usuario;
+      this.nombre = opciones.nombre;
+      this.email = opciones.email;
+      this.password = opciones.password;
+      this.saludar = function () {
+        return "Hola, mi nombre es " + this.nombre;
+      };
     }
   }
-  return Usuario
+  return Usuario;
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
-  Constructor.prototype.saludar=function(){
-    return "Hello World!"
-  }
+  Constructor.prototype.saludar = function () {
+    return "Hello World!";
+  };
 }
 
 function agregarStringInvertida() {
@@ -47,39 +47,39 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-  String.prototype.reverse=function(){
-    var stringInvertida= ""
-    for (var i=this.length-1; i>=0; i--){
-      stringInvertida= stringInvertida+this[i]
-    } 
-   return stringInvertida
-  }
+  String.prototype.reverse = function () {
+    var stringInvertida = "";
+    for (var i = this.length - 1; i >= 0; i--) {
+      stringInvertida = stringInvertida + this[i];
+    }
+    return stringInvertida;
+  };
 }
 
 // ---------------------------------------------------------------------------//
-  //Crea el constructor de la clase "Persona"
-  //Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio"
-  //Debe tener un método llamado "detalle" que nos devuelve un objeto con las propiedades de la persona y sus valores.
-  //Ej: { 
-    //   Nombre: 'Juan',
-    //   Apellido: 'Perez',
-    //   Edad: 22,
-    //   Domicilio: 'Saavedra 123'
-    //  }
+//Crea el constructor de la clase "Persona"
+//Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio"
+//Debe tener un método llamado "detalle" que nos devuelve un objeto con las propiedades de la persona y sus valores.
+//Ej: {
+//   Nombre: 'Juan',
+//   Apellido: 'Perez',
+//   Edad: 22,
+//   Domicilio: 'Saavedra 123'
+//  }
 
-  class Persona {
-    constructor(nombre, apellido, edad, domicilio) {
-      // Crea el constructor:
-      this.nombre= nombre;
-      this.apellido=apellido;
-      this.edad=edad;
-      this.domicilio=domicilio;
-      this.detalle=function(){
-        return {
-          Nombre: this.nombre,
-          Apellido: this.apellido,
-          Edad: this.edad,
-          Domicilio:this.domicilio,
+class Persona {
+  constructor(nombre, apellido, edad, domicilio) {
+    // Crea el constructor:
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.domicilio = domicilio;
+    this.detalle = function () {
+      return {
+        Nombre: this.nombre,
+        Apellido: this.apellido,
+        Edad: this.edad,
+        Domicilio: this.domicilio,
       };
     };
   }
@@ -89,21 +89,20 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
-  var persona1=new Persona(nombre, apellido, edad, dir)
-  return persona1
+  var persona1 = new Persona(nombre, apellido, edad, dir);
+  return persona1;
 }
-  
+
 function agregarMetodo() {
-  //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
+  //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve:
   //Ej: "Juan, 22 años"
   //Persona.prototype.datos=function(){
   //  return this.nombre+ ", "+this.edad+" años";
   //};
-  Persona.prototype.datos=function(){
+  Persona.prototype.datos = function () {
     return `${this.nombre}, ${this.edad} años`;
   };
 }
-  
 
 // No modificar nada debajo de esta línea
 // --------------------------------
@@ -113,6 +112,6 @@ module.exports = {
   agregarMetodoPrototype,
   agregarStringInvertida,
   crearInstanciaPersona,
-  agregarMetodo, 
-  Persona
+  agregarMetodo,
+  Persona,
 };
--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,30 +4,54 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var mayuscula=nombre[0].toUpperCase()
+  var letras=nombre.slice(1);
+  return mayuscula + letras;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  //cb --> function pepito(){
+  //  console.log('hola');
+  //}
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  cb(n1, n2);
 }
+//operacionMatematica(1, 5, function(){
+//  return a+b;
+//})
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  //numeros =[1, 2, 3, 4]
+  //var resultado = 0;
+  //for(let i=0;i<numeros.length; i++){
+  //  resultado=resultado + numeros[i];
+  //}
+  //cb(resultado);
+  var resultado = numeros.reduce(function(acumulador,valorActual){
+    return acumulador + valorActual;
+  })
+    cb(resultado);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(elemento){
+    cb(elemento);
+  })
 }
 
 function map(array, cb) {
@@ -35,12 +59,20 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  let nuevoArray= array.map(function(otroElemento){
+    return cb(otroElemento)
+  })
+  return nuevoArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  let nuevoArray=array.filter(function(x){
+    return x[0] === 'a';
+  })
+  return nuevoArray
 }
 
 // No modificar nada debajo de esta línea
